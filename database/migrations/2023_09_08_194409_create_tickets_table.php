c706f34da20ac4f46b9bcc2225f2c94c344cbe29
sjaras <sjara@mined.academy>
2023-09-14 02:26:03 -0500

    cambios en clientes

--- database/migrations/2023_09_08_194409_create_tickets_table.php
+++ database/migrations/2023_09_08_194409_create_tickets_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
             $table->string('subject');
+            $table->string('description', 255);
             $table->enum('priority', ['baja', 'media', 'alta'])->default('baja');
             $table->enum('status', ['derivación al area especializada', 'en proceso', 'resuelto'])
                 ->default('derivación al area especializada');
--- resources/views/client/tickets/create.blade.php
+++ resources/views/client/tickets/create.blade.php
@@ -1,25 +1,27 @@
 @extends('layouts.ticketslayot')
-@section('titulo-seccion','Enviar un ticket')
-@section('contenido') 
-<form class="login">
-  <div class="login__body">
+@section('titulo-seccion', 'Enviar un ticket')
+@section('contenido')
+    <form class="login" method="POST" action="{{ url('app/client/tickets') }}">
+        @csrf
+        {{-- @method('PUT') --}}
+        <div class="login__body">
 
-      <div class="login__campo">
-          <label class="login__label" for="correo">
-              <span class="login__span">Asunto:</span>
-              <input class="login__input" id="username" type="text" name="asunto" />
-          </label>
-      </div>
+            <div class="login__campo">
+                <label class="login__label" for="correo">
+                    <span class="login__span">Asunto:</span>
+                    <input class="login__input" id="username" type="text" name="asunto" />
+                </label>
+            </div>
 
-      <div class="login__campo">
-          <label class="login__label" for="password">
-              <span class="login__span">Descripcion:</span>
-              <textarea class="form__descripcion" name="descripcion"></textarea>
-          </label>
-      </div>
+            <div class="login__campo">
+                <label class="login__label" for="password">
+                    <span class="login__span">Descripcion:</span>
+                    <textarea class="form__descripcion" name="descripcion"></textarea>
+                </label>
+            </div>
 
-      <input class="login__button" type="submit" value="Enviar" />
+            <input class="login__button" type="submit" value="Enviar" />
 
-  </div>
-</form>
+        </div>
+    </form>
 @endsection
