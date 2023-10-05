fcac7e60e7a10bd5334d4979517880d5cc95013d
Erick Ramirez <erickramirez.dnbk@gmail.com>
2023-10-05 08:08:56 -0500

    input para buscar tickets agregado en listado de tickets

--- resources/views/client/tickets/index.blade.php
+++ resources/views/client/tickets/index.blade.php
@@ -1,11 +1,18 @@
 @extends('layouts.ticketslayot')
 @section('titulo-seccion', 'Tickets')
 @section('contenido')
+    <form action="" method="get" style="margin: 0.25cm; width: 50%">
+        <label for="tickets-search-term" class="login__label">
+            <input class="login__input" type="search" name="tickets-search-term" id="tickets-search-term"
+                placeholder="Buscar ticket">
+        </label>
+    </form>
     <div class="ticket__container">
         @foreach ($tickets as $ticket)
             <div class="ticket">
                 <div class="ticket__body">
-                    <div class="ticket__logo"><img src="{{ asset('assets/client/img/logo-ticket.png') }}" alt=""></div>
+                    <div class="ticket__logo"><img src="{{ asset('assets/client/img/logo-ticket.png') }}" alt="">
+                    </div>
                     <div class="ticket__texts">
                         <a href="{{ url('app/client/tickets/' . $ticket->id) }}"
                             class="ticket__titulo">{{ $ticket->subject }}</a>
