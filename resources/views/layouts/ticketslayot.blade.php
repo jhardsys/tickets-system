8cf04d57d007a4f686ad2ae622ec460950c4f881
sjaras <sjara@mined.academy>
2023-10-25 23:43:43 -0500

    add layout

--- resources/views/layouts/ticketslayot.blade.php
+++ resources/views/layouts/ticketslayot.blade.php
@@ -2,69 +2,150 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('assets/client/style.css') }}">
-    <!-- Unicons CSS -->
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
-    {{-- Google fonts --}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+    <!-- Style.css -->
+    <link href="{{ asset('assets/admin/style.css') }}" rel="stylesheet" />
+    <!-- Box icons -->
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+    {{-- Tailwind --}}
+    <script src="https://cdn.tailwindcss.com"></script>
 
-    <title>@yield('title')</title>
+    {{-- Alpine js --}}
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+    <title>Box icons</title>
 </head>
 
 <body>
-    <header class="header">
-        <nav class="nav">
-            {{-- <i class="uil uil-bars navOpenBtn"></i> --}}
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                class="bi bi-list navOpenBtn" viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
-                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-            </svg>
-            <a href="{{ url('app/client/tickets') }}" class="logo">SYSTICKETS</a>
-            <ul class="nav-links">
-                <i class="uil uil-times navCloseBtn"></i>
+    <div class="sidebar">
+        <a class="logo-details" href="{{ route('admin.tickets.index') }}">
+            <i class="bx bxl-c-plus-plus"></i>
+            <span class="logo_name">SYSTICKETS</span>
+        </a>
 
-                <li><a href="{{ url('app/client/tickets') }}">Inicio</a></li>
-                <li><a href="{{ url('app/client/tickets') }}" class="nav__link">Tickets</a></li>
-                <li><a href="{{ url('app/client/tickets/create') }}" class="nav__link-enviar-ticket">Enviar un
-                        ticket</a></li>
-                <form method="POST" action="{{ route('logout.store') }}">
-                    @csrf
-                    <button type="submit" class="nav__link">Cerrar sesión</button>
-                </form>
-            </ul>
+        <ul class="nav-links">
+            <li>
+                <div class="iocn-link">
+                    <a href="{{ route('admin.tickets.index') }}">
+                        <i class="bx bx-collection"></i>
+                        <span class="link_name">Ticket</span>
+                    </a>
+                    <i class="bx bx-chevron-down arrow"></i>
+                </div>
+                <ul class="sub-menu">
+                    <li><a href="#" class="link_name">Ticket</a></li>
+                    <li><a href="{{ route('admin.tickets.index') }}">Listar tickets</a></li>
+                    <li><a href="{{ route('admin.tickets.index') }}">Crear nuevo ticket</a></li>
 
-            <div class="perfil" id="perfilContainer">
-                <a class="perfil__a" href="{{ route('client.perfil.index') }}">
-                    <p>P</p>
+                </ul>
+            </li>
+            <li>
+                <div class="iocn-link">
+                    <a href="{{ url('app/admin/clients') }}">
+                        <i class='bx bx-book-alt'></i>
+                        <span class="link_name">Clientes</span>
+                    </a>
+                    <i class="bx bx-chevron-down arrow"></i>
+                </div>
+                <ul class="sub-menu">
+                    <li><a href="" class="link_name">Clientes</a></li>
+                    <li><a href="{{ url('app/admin/clients/create') }}">Registrar cliente</a></li>
+                    <li><a href="{{ route('admin.clients.index') }}">Listar clientes</a></li>
+                    <li><a href="#">Listar clientes no autorizados</a></li>
+                    {{-- <li><a href="{{ url('app/admin/clients/update') }}">Actualizar cliente</a></li> --}}
+                </ul>
+            </li>
+            <li>
+                <div class="iocn-link">
+                    <a href="#">
+                        <i class='bx bx-book-alt'></i>
+                        <span class="link_name">Agentes</span>
+                    </a>
+                    <i class="bx bx-chevron-down arrow"></i>
+                </div>
+                <ul class="sub-menu">
+                    <li><a href="#" class="link_name">Agentes</a></li>
+                    <li><a href="#">Listar agentes</a></li>
+                    <li><a href="#">Registrar agente</a></li>
+                    {{-- <li><a href="{{ url('app/admin/clients/update') }}">Actualizar cliente</a></li> --}}
+                </ul>
+            </li>
+
+            <li>
+                <a href="{{ route('admin.perfil.index') }}">
+                    <i class='bx bx-user'></i>
+                    <span class="link_name">Profile</span>
                 </a>
-            </div>
-        </nav>
-    </header>
+                <ul class="sub-menu blank">
+                    <li><a href="{{ route('admin.perfil.index') }}" class="link_name">Profile</a></li>
+                </ul>
+            </li>
+            {{-- <li>
+                <a href="#">
+                    <i class='bx bx-cog'></i>
+                    <span class="link_name">Setting</span>
+                </a>
+                <ul class="sub-menu blank">
+                    <li><a href="#" class="link_name">Setting</a></li>
+                </ul>
+            </li> --}}
+            <li>
+                <div class="profile-details">
+                    <a href="{{ route('admin.perfil.index') }}">
 
-    <main class="main">
-        <div class="head" style="flex-direction: column;">
-            <div class="head__ticket">
-                @yield('titulo-seccion')
-            </div>
-            <div style="font-size: 16px; font-weight: 100;">
-                @yield('description')
-            </div>
+                        <div class="profile-content">
+                            <img src="{{ asset('assets/img/admin/profile-icon.avif') }}" alt="profile">
+                        </div>
+                        <div class="name-job">
+                            <div class="profile_name">
+                                {{ session()->get('user_session')['first_name'] }}
+                            </div>
+                            {{-- <div class="job">Web Designer</div> --}}
+                        </div>
+                    </a>
+
+                    <form action="{{ route('logout.store') }}" method="POST">
+                        @csrf
+                        <button type="submit" style="color: transparent; background-color:transparent; border:none">
+                            <i class='bx bx-log-out'></i>
+                        </button>
+                    </form>
+                </div>
+
+            </li>
+        </ul>
+    </div>
+    <header class="home-section">
+        <div class="home-content bg-white">
+            <i class='bx bx-menu'></i>
+            <span class="text">Sistema de tickets</span>
         </div>
-
-        @yield('contenido')
+    </header>
+    <main class="main">
+        @yield('content')
     </main>
-
-    <footer class="footer">
-        <p class="footer__name">Software de Help Desk de Freshdesk</p>
-        <b class="footer__privacy">Politica de privacidad</b>
-    </footer>
-    <script src="{{ asset('assets/client/script.js') }}"></script>
+    @stack('script')
 </body>
+
+<script>
+    let arrows = document.querySelectorAll(".arrow");
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".bx-menu");
+    arrows.forEach(arrow => {
+        arrow.addEventListener("click", (e) => {
+            let arrowParent = e.target.parentElement.parentElement;
+            arrowParent.classList.toggle("showMenu");
+        });
+    });
+
+    sidebarBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+    })
+</script>
 
 </html>
